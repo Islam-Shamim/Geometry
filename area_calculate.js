5d03c62d20ac839847e3dda5342ea216a10af885
Islam-Shamim <islamshamim1819@gmail.com>
2024-02-24 10:45:59 +0600

    final

--- area_calculate.js
+++ area_calculate.js
@@ -11,9 +11,16 @@ function calculateTriangleArea(){
     const heightValue = parseFloat(heightValueText);
 
     //triangle area
+    if(isNaN(baseValue)||isNaN(heightValue)){
+        alert("Please insert number");
+        return
+    }
     const area = 0.5 * baseValue * heightValue;
     const triArea = document.getElementById("triangleArea");
     triArea.innerText = area;
+
+    //add to calculation entry
+    addToCalculationEntry('Triangle',area);
 
 }
 
@@ -24,15 +31,22 @@ function calculateRectangleArea(){
     const widthValueText = widthField.value;
     const widthValue = parseFloat(widthValueText);
 
-    //get rectangle lenght value
+    //get rectangle length value
     const lenghtField = document.getElementById('r-lenght');
     const lenghtValueText = lenghtField.value;
     const lenghtValue = parseFloat(lenghtValueText);
 
     //rectangle area
+    if(isNaN(widthValue)||isNaN(lenghtValue)){
+        alert("Please insert number");
+        return
+    }
     const area = widthValue * lenghtValue;
     const triArea = document.getElementById("rectangleArea");
     triArea.innerText = area;
+
+    //add to calculation entry
+    addToCalculationEntry('Rectangle',area);
 }
 
 //ellipse
@@ -48,9 +62,16 @@ function calculateEllipseArea(){
     const majorValue = parseFloat(majorValueText);
 
     //ellipse area
+    if(isNaN(minorValue)||isNaN(majorValue)){
+        alert("Please insert number");
+        return
+    }
     const area = 3.1416 * minorValue * majorValue;
     const triArea = document.getElementById("ellipseArea");
     triArea.innerText = area;
+
+    //add to calculation entry
+    addToCalculationEntry('Ellipse',area);
 }
 
 //function reuseable
@@ -63,22 +84,36 @@ function calculateParallelogramArea(){
     //get parallelogram height value
     const height = getInputValue('p-height');
 
+    if(isNaN(base)||isNaN(height)){
+        alert("Please insert number");
+        return
+    }
     //parallelogram area
     const area = base * height;
     setArea('parallelogramArea',area);
 
+    //add to calculation entry
+    addToCalculationEntry('Parallelogram',area);
+
 }
 //rhombos
 function calculateRhombusArea(){
-    //get parallelogram base value
+    //get rhombos base value
     const diagonal1 = getInputValue('r-d1');
     
-    //get parallelogram height value
+    //get rhombos height value
     const diagonal2 = getInputValue('r-d2');
 
-    //parallelogram area
+    if(isNaN(diagonal1)||isNaN(diagonal2)){
+        alert("Please insert number");
+        return
+    }
+    //rhombos area
     const area = 0.5 * diagonal1 * diagonal2;
     setArea('rhombusArea',area);
+
+    //add to calculation entry
+    addToCalculationEntry('Rhombos',area);
 
 }
 
@@ -94,4 +129,25 @@ function getInputValue(fieldId){
 function setArea(areaId,area){
     const areaField = document.getElementById(areaId);
     areaField.innerText = area;
+}
+
+
+
+//add to calculation entry
+/*
+1. get the element where you want to add the dynamic html
+2. creat an element that you want to add
+3. append the created element as a child of the parent
+*/
+function addToCalculationEntry(areaType,area){
+    //parent
+    const calculationEntry = document.getElementById('calculation-entry');
+
+    const count = calculationEntry.childElementCount;
+    //create child
+    const p = document.createElement('p');
+    p.classList.add('my-4');
+    p.innerHTML = `${count+1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success ">convert</button>`;
+    //append child with parent
+    calculationEntry.appendChild(p);
 }
